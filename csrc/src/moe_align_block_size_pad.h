c8a85cc6a6fc3bb6907405889d8c3151e09defcc
xin.guo <xin.guo@enflame-tech.com>
2025-02-27 20:49:55 +0800

    [Feature](ARD-2923) vllm: add ops
    Root Cause:
            1
    Solution:
            1
    Test:
            1
    Impact area:
            1
    Fix status:
            1
    
    Change-Id: I3e51b776db591f09b0e231b1717860582196d0b7

---
+++ csrc/src/moe_align_block_size_pad.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2025 Enflame. All Rights Reserved.
+ */
+#pragma once
+
+#include <ATen/ATen.h>
+
+namespace vllm_gcu::llm_ops {
+
+void moe_align_block_size_pad(at::Tensor topk_ids, at::Tensor topk_ids_size,
+                              int64_t num_experts, int64_t block_size,
+                              at::Tensor sorted_token_ids,
+                              at::Tensor experts_ids,
+                              at::Tensor num_tokens_post_pad);
+
+}  // namespace vllm_gcu::llm_ops
