fdaa399b4c0574000e54debf9be3cfdb4a4ccf87
sp.yancong.liu <sp.yancong.liu@enflame-tech.com>
2025-03-03 14:48:07 +0800

    [Feature](TR-57018) vllm: Add vision language model test
    Root Cause:
            N/A
    Solution:
            N/A
    Test:
            N/A
    Impact area:
            vllm
    Fix status:
            N/A
    
    Signed-off-by: sp.yancong.liu <sp.yancong.liu@enflame-tech.com>
    Change-Id: I6ad3913c64b931cd3745aac20db7cf449c31f0f5

---
+++ benchmarks/vision_language/datasets/__init__.py
@@ -0,0 +1,14 @@
+import importlib
+
+VLM_DATASETS = {
+    "llava-bench-coco": ("llava_bench_coco", "LlavaBenchCocoDataset"),
+    "MMMU": ("mmmu", "MMMUDataset"),
+    "videomme":("videomme", "VideoMMEDataset")
+}
+
+
+def resolve_dataset_cls(dataset_name: str):
+    dataset, dataset_cls_name = VLM_DATASETS[dataset_name]
+    module = importlib.import_module(
+        f"vllm_utils.vision_language.datasets.{dataset}")
+    return getattr(module, dataset_cls_name, None)
