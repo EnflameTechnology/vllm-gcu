ae931707636f057c6e2de5035672a997a8b27501
hongze.tian <hongze.tian@enflame-tech.com>
2025-03-14 20:51:55 +0800

    [Fix](ARD-2838) ard: dsv3 (mla) remove 1 fill op
    Root Cause:
            as title
    Solution:
            zeros -> empty
    Test:
            gcu
    Impact area:
            mla
    Fix status:
            N/A
    
    Change-Id: Id42c1da28468a5d5e084e8a16efc95db0b2b7636

--- vllm_gcu/attention/backends/mla.py
+++ vllm_gcu/attention/backends/mla.py
@@ -959,7 +959,7 @@ class GCUMLAImpl(MLACommonImpl[GCUMLAMetadata]):
         B = q_nope.shape[0]
 
         q = torch.cat([q_nope, q_pe], dim=-1)
-        o = torch.zeros(
+        o = torch.empty(
             B, self.num_heads, self.kv_lora_rank, dtype=q.dtype, device=q.device
         )
 
