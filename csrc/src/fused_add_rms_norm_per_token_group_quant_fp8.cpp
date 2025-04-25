8fff16e51ef8e9c056bf7ed776ba44c9ece8b505
dean.zhang <dean.zhang@enflame-tech.com>
2025-04-25 14:49:31 +0800

    [Feature](ARD-2916) vllm: add libra ops
    Root Cause:
            add libra ops
    Solution:
            as above
    Test:
            gcusim
    Impact area:
            vllm
    Fix status:
            N/A
    
    Signed-off-by: dean.zhang <dean.zhang@enflame-tech.com>
    Change-Id: Ibb0f58e5836955aae7a97c4bc276b1739114176b
    Signed-off-by: dean.zhang <dean.zhang@enflame-tech.com>

---
+++ csrc/src/fused_add_rms_norm_per_token_group_quant_fp8.cpp
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2024 Enflame. All Rights Reserved.
+ */
+#include "fused_add_rms_norm_per_token_group_quant_fp8.h"
+
+#include <topsaten/topsaten_vllm.h>
+
+#include "tops_extension/torch/GCUAten.h"
+#include "torch_gcu.h"
+
+namespace vllm_gcu::llm_ops {
+void fused_add_rms_norm_per_token_group_quant_fp8(
+    at::Tensor &out,
+    at::Tensor &residual_update,
+    at::Tensor &scale,
+    const at::Tensor &input,
+    const at::Tensor &residual,
+    const at::Tensor &weight,
+    double epsilon,
+    int64_t group_size) {
+  const torch_gcu::OptionalGCUGuard device_guard(device_of(out));
+  const topsStream_t stream = torch_gcu::getCurrentGCUStream();
+
+  ATEN_ATENOP_CHECK(
+      ATEN_ATENOP_CALL(topsvllm::topsvllmFusedAddRmsNormPerTokenGroupQuantFp8)(
+          out, residual_update, scale, input, residual, weight,
+          epsilon, group_size, stream));
+}
+
+} // namespace vllm_gcu::llm_ops
