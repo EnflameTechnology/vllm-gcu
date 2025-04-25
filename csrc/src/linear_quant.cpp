2ea10139f73ddebaa2c785c70708e0a1956653e0
dean.zhang <dean.zhang@enflame-tech.com>
2025-04-25 14:49:31 +0800

    [Feature](ARD-2916) vllm: add libra ops
    Root Cause:
            add libra ops
    Solution:
            as above
    Test:
            gcusim
    Impact area:
            vllm
    Fix status:
            N/A
    
    Signed-off-by: dean.zhang <dean.zhang@enflame-tech.com>
    Change-Id: Ibb0f58e5836955aae7a97c4bc276b1739114176b
    Signed-off-by: dean.zhang <dean.zhang@enflame-tech.com>

---
+++ csrc/src/linear_quant.cpp
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2024 Enflame. All Rights Reserved.
+ */
+#include "linear_quant.h"
+
+#include <topsaten/topsaten_vllm.h>
+
+#include "tops_extension/torch/GCUAten.h"
+#include "torch_gcu.h"
+
+namespace vllm_gcu::llm_ops {
+void linear_quant(at::Tensor &out, const at::Tensor &lhs,
+                 const at::Tensor &rhs, const c10::optional<at::Tensor> &bias,
+                 const at::Tensor &lhs_scale,
+                 const at::Tensor &rhs_scale) {
+  const torch_gcu::OptionalGCUGuard device_guard(device_of(out));
+  const topsStream_t stream = torch_gcu::getCurrentGCUStream();
+
+  at::Tensor bias_tensor;
+  if (bias.has_value()) {
+    bias_tensor = bias.value();
+  }
+
+  ATEN_ATENOP_CHECK(
+      ATEN_ATENOP_CALL(topsaten::topsatenLinearQuant)(
+          out, lhs, rhs, bias_tensor, lhs_scale, rhs_scale, stream));
+}
+
+} // namespace vllm_gcu::llm_ops
