b2a7013fd63a48612f1fb456879a864704d3c5ee
tianyu.jiang <tianyu.jiang@enflame-tech.com>
2025-04-29 15:15:58 +0800

    [Feature](ARD-2916) vllm: add compile
    Root Cause:
            use inductor
    Solution:
            use inductor
    Test:
            gcu
    Impact area:
            libra
    Fix status:
            Done
    
    Change-Id: Ic06f7aabcb6a353f5f33c035c857e99e6e4a0f88

--- vllm_gcu/attention/__init__.py
+++
@@ -1,6 +0,0 @@
-from vllm.utils import vllm_lib
-
-
-for opdef, handle in zip(vllm_lib._op_defs, vllm_lib._registration_handles):
-    if opdef == "vllm::unified_attention":
-        handle.destroy()
