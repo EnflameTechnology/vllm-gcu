40907532cbf0aac99659caa998cf254a135d98ad
dean.zhang <dean.zhang@enflame-tech.com>
2025-05-20 11:14:54 +0800

    [Feature](ARD-2987) vllm: fix libra ops for compile
    Root Cause:
            fix libra ops
    Solution:
            as above
    Test:
            gcusim
    Impact area:
            vllm
    Fix status:
            N/A
    
    Signed-off-by: dean.zhang <dean.zhang@enflame-tech.com>
    Change-Id: I0f18e62708f21030eeeaa32d1d1323ab6a9d9dc2

--- csrc/src/rms_norm_static_int8_quant.cpp
+++ csrc/src/rms_norm_static_int8_quant.cpp
@@ -1,7 +1,7 @@
 /**
  * Copyright 2024 Enflame. All Rights Reserved.
  */
-#include "static_scaled_int8_quant.h"
+#include "rms_norm_static_int8_quant.h"
 
 #include <topsaten/topsaten_vllm.h>
 
@@ -9,9 +9,9 @@
 #include "torch_gcu.h"
 
 namespace vllm_gcu::llm_ops {
-void static_scaled_int8_quant(at::Tensor& output, const at::Tensor& input,
-                              const at::Tensor& scale,
-                              const ::std::optional<at::Tensor>& azp) {
+void rms_norm_static_int8_quant(at::Tensor& output, const at::Tensor& input,
+                                const at::Tensor& weight,
+                                const at::Tensor& scale, double epsilon) {
   const torch_gcu::OptionalGCUGuard device_guard(device_of(output));
   const topsStream_t stream = torch_gcu::getCurrentGCUStream();
   at::Tensor in_scale;
@@ -22,9 +22,10 @@ void static_scaled_int8_quant(at::Tensor& output, const at::Tensor& input,
     // per channel
     in_scale = scale.reciprocal().to(input.dtype());
   }
+  at::Scalar scalar_epsilon(epsilon);
 
-  ATEN_ATENOP_CHECK(ATEN_ATENOP_CALL(topsaten::topsatenQuantize)(
-      output, input, in_scale, stream));
+  ATEN_ATENOP_CHECK(ATEN_ATENOP_CALL(topsvllm::topsvllmRmsNormQuant)(
+      output, input, weight, in_scale, scalar_epsilon, stream));
 }
 
 }  // namespace vllm_gcu::llm_ops
