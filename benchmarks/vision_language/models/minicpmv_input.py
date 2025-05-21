94db02eff7e8722049990780b2f2a5aefdb20d6e
steven.zhang <steven.zhang@enflame-tech.com>
2025-05-21 17:30:35 +0800

    [Fix](TR-58421) vllm: fix minicpmv-2.6 error
    Root Cause:
            code difference caused by vllm's upgrade to 0.8.0
    Solution:
            copy code from vllm 0.6.1.post2
    Test:
            S60
    Impact area:
            vllm
    Fix status:
            done
    
    Signed-off-by: steven.zhang <steven.zhang@enflame-tech.com>
    Change-Id: Ic750d1e8a9a3485bed5d6e04cb29cde5ff7440ec

--- benchmarks/vision_language/models/minicpmv_input.py
+++ benchmarks/vision_language/models/minicpmv_input.py
@@ -4,12 +4,14 @@ from typing import Optional, List, Union
 from decord import VideoReader, cpu
 from PIL import Image
 from vllm_utils.vision_language.models.base import VLMInput
-from vllm.model_executor.models.minicpmv import input_processor_for_minicpmv
-from vllm.inputs.registry import InputContext
-from vllm.config import ModelConfig
+from transformers import AutoProcessor
 
 
 class MinicpmvImageInput(VLMInput):
+    def __init__(self, model: str, tokenizer: Optional[str]):
+        super().__init__(model, tokenizer)
+        self.processor = AutoProcessor.from_pretrained(self.model, trust_remote_code=True)
+        self.image_processor = self.processor.image_processor
 
     def get_demo_prompt(self, question: str or List, **kwargs):
         mm_per_prompt = kwargs[
@@ -61,71 +63,39 @@ class MinicpmvImageInput(VLMInput):
             raise ValueError(
                 'mm_per_prompt must be equal to input_vision_shape group')
 
-        from vllm.multimodal.image import cached_get_image_processor
-        image_processor = cached_get_image_processor(self.model,
-                                                     trust_remote_code=True)
-
-        max_slice_nums = 1 if len(vision_shapes) > 16 else 2
         image_feature_sizes = []
         for i in range(len(vision_shapes)):
             input_shape = vision_shapes[i].split(",")
             input_height = int(input_shape[-2])
             input_width = int(input_shape[-1])
 
-            placeholder = image_processor. \
-            get_slice_image_placeholder(
+            placeholder = self.image_processor.get_slice_image_placeholder(
                 (input_height, input_width),
                 i,
-                use_image_id=False,
-                max_slice_nums=max_slice_nums
-            )
+                )
 
             image_feature_sizes.append(len(self.tokenizer.encode(placeholder)))
 
         return image_feature_sizes
 
     def get_dummy_prompt(self, input_len: int, placeholder: str,
-                         image_feature_size: int,
+                         image_feature_sizes:  List[int],
                          **kwargs):
 
         mm_per_prompt = kwargs["mm_per_prompt"] if kwargs[
             "mm_per_prompt"] else 1
 
-        input_vision_shape = kwargs["input_vision_shape"]
-        kwargs.pop("input_vision_shape")
+        prompt = self.get_demo_prompt("", **kwargs)
+        prompt_len = len(self.tokenizer.encode(prompt)) - (len(self.tokenizer.encode("(<image>./</image>)"))-1) * mm_per_prompt
 
-        model_config = ModelConfig(
-            model=self.model,
-            tokenizer=self.model,
-            tokenizer_mode="auto",
-            trust_remote_code=True,
-            seed=0,
-            dtype="auto",
-            revision=None,
-            limit_mm_per_prompt={"image": mm_per_prompt},
-        )
-        prompt = self.get_demo_prompt("hi", **kwargs)
-        vision_data = self.get_dummy_vision_data(input_vision_shape, **kwargs)
+        total_image_feature_size = sum(image_feature_sizes)
+        dummy_token_len = input_len - (prompt_len + total_image_feature_size)
 
-        input = {
-            "prompt": prompt,
-            "multi_modal_data": {
-                "image": vision_data["image"]
-            }
-        }
-        data = input_processor_for_minicpmv(InputContext(model_config), input)
-        fake_prompt_len = input_len - len(data["prompt_token_ids"])
-        fake_prompt = "hi" * fake_prompt_len
-        all_prompt = prompt + fake_prompt
-        return all_prompt
+        question = "hi" * (dummy_token_len)
+        prompt = self.get_demo_prompt(question, **kwargs)
 
-    def get_stop_token_ids(self):
-        stop_tokens = ['<|im_end|>', '<|endoftext|>']
-        stop_token_ids = [
-            self.tokenizer.convert_tokens_to_ids(i) for i in stop_tokens
-        ]
-        return stop_token_ids
-   
+        return prompt
+
     def get_dummy_vision_data(self,
                               input_vision_shape: str,
                               **kwargs):
@@ -147,10 +117,17 @@ class MinicpmvImageInput(VLMInput):
         return mm_data
 
     def get_placeholder(self):
-        image_token_id = 151646
-        image_place_holder = self.tokenizer.decode(image_token_id)
-        return image_place_holder
+        return "(<image>./</image>)"
 
+    def dataset_pred_post_process(self,
+                                  dataset_name: str,
+                                  pred: str):
+        parsed_pred = pred.split("\n")[0].strip()
+        all_choices = ["A", "B", "C", "D"]
+        for choice in all_choices:
+            if '{})'.format(choice) in parsed_pred:
+                parsed_pred = choice
+        return parsed_pred
 
 class MinicpmvVideoInput(MinicpmvImageInput):
 
@@ -158,50 +135,17 @@ class MinicpmvVideoInput(MinicpmvImageInput):
         super().__init__(model, tokenizer)
         self.modality = "video"
 
-    def get_demo_prompt(self, question, **kwargs):
-        num_frame = kwargs["num_frames"] if "num_frames" in kwargs.keys(
-        ) else 8
-
+    def get_demo_prompt(self, question : str, **kwargs):
         # demo
         messages = [{
-            'role':
-            'user',
-            "content":
-            "".join(["(<image>./</image>)"] * num_frame) + "\n" + question
+            'role': 'user',
+            "content": "".join(["(<video>./</video>)"]) + "\n" + question
         }]
 
         prompt = self.tokenizer.apply_chat_template(
             messages, tokenize=False, add_generation_prompt=True)
         return prompt
-
-    def get_demo_vision_data(self, input_vision_file: str, **kwargs):
-        num_frame = kwargs["num_frames"] if kwargs["num_frames"] else 8
-
-        def encode_video(filepath):
-
-            def uniform_sample(l, n):
-                gap = len(l) / n
-                idxs = [int(i * gap + gap / 2) for i in range(n)]
-                return [l[i] for i in idxs]
-
-            vr = VideoReader(filepath, ctx=cpu(0))
-            sample_fps = round(vr.get_avg_fps() / 1)  # FPS
-            frame_idx = [i for i in range(0, len(vr), sample_fps)]
-            if len(frame_idx) > num_frame:
-                frame_idx = uniform_sample(frame_idx, num_frame)
-            video = vr.get_batch(frame_idx).asnumpy()
-            video = [Image.fromarray(v.astype('uint8')) for v in video]
-            return video
-
-        image_data = encode_video(input_vision_file)
-        return {
-            "image": {
-                "images": image_data,
-                "use_image_id": False,
-                "max_slice_nums": 1 if num_frame > 16 else 2
-            }
-        }
-
+    
     def dataset_pre_process(self,
                             dataset_name: str,
                             question: Union[str, List],
@@ -230,33 +174,19 @@ class MinicpmvVideoInput(MinicpmvImageInput):
             messages = [msg]
             prompt = self.tokenizer.apply_chat_template(
                 messages, tokenize=False, add_generation_prompt=True)
-            vision_data = {"image": {"images":images,
-                        "use_image_id":False,
-                        "max_slice_nums": 1 if len(images) > 16 else 2}}
+            vision_data = {"image": images}
             return prompt, vision_data
         else:
             raise ValueError(f"Unsupported dataset {dataset_name}.")
 
-    def get_dummy_vision_data(self,
-                              input_vision_shape: str,
-                              **kwargs):
-        vision_shapes = input_vision_shape.split(";")
-        if kwargs["mm_per_prompt"] != len(vision_shapes):
-            raise ValueError('mm_per_prompt must be equal to input_vision_shape group')
+    def get_demo_vision_data(self, input_vision_file: str, **kwargs):
+        from vllm.assets.video import VideoAsset
 
-        pil_images = []
-        for vision_shape in vision_shapes:
-            input_shape = vision_shape.split(",")
-            image_height = int(input_shape[-2])
-            image_width = int(input_shape[-1])
-            pil_image = Image.new("RGB", (image_width, image_height), color=0)
-            pil_images.append(pil_image)
+        video = VideoAsset(
+            name=input_vision_file,
+                           num_frames=kwargs["num_frames"]).np_ndarrays
 
-        mm_data = {"image":
-        {
-            "images": pil_images,
-            "use_image_id": False,
-            "max_slice_nums": 1 if kwargs["mm_per_prompt"] > 16 else 2   
-        }
-        }
-        return mm_data
+        return {self.modality: video}
+
+    def get_placeholder(self):
+        return "(<video>./</video>)"
