d6134492c853969ecb2a383e42d4156fdbecf6d6
tianyu.jiang <tianyu.jiang@enflame-tech.com>
2025-05-21 11:19:24 +0800

    [Test](ARD-2987) vllm: add dump node in compile graph
    Root Cause:
            add debug method
    Solution:
            as above
    Test:
            Libra
    Impact area:
            vllm compile
    Fix status:
            Done
    
    Change-Id: I8287b751ce0a1bc45f44fa4eb54e079709210f8b

--- vllm_gcu/envs.py
+++ vllm_gcu/envs.py
@@ -7,15 +7,21 @@ environment_variables: Dict[str, Callable[[], Any]] = {
     ),
     "VLLM_GCU_SAMPLER_ON_CPU": lambda: int(os.getenv("VLLM_GCU_SAMPLER_ON_CPU", "0")),
     "VLLM_DUMP_SNAPSHOT_EVERY_N_STEP": lambda: int(
-        os.getenv("VLLM_DUMP_SNAPSHOT_EVERY_N_STEP", 0)
+        os.getenv("VLLM_DUMP_SNAPSHOT_EVERY_N_STEP", "0")
     ),
-    "VLLM_GCU_RANK_LOG_PATH": lambda: (None if os.getenv("VLLM_GCU_RANK_LOG_PATH", None) is None else os
-                                       .path.expanduser(os.getenv("VLLM_GCU_RANK_LOG_PATH", "."))),
+    "VLLM_GCU_RANK_LOG_PATH": lambda: (
+        None
+        if os.getenv("VLLM_GCU_RANK_LOG_PATH", None) is None
+        else os.path.expanduser(os.getenv("VLLM_GCU_RANK_LOG_PATH", "."))
+    ),
     "VLLM_GCU_ENABLE_PARALLEL_COMPUTE": lambda: bool(
         int(os.getenv("VLLM_GCU_ENABLE_PARALLEL_COMPUTE", "0"))
     ),
     "VLLM_GCU_DEEPSEEK_FUSION": lambda: bool(
-        int(os.getenv("VLLM_GCU_DEEPSEEK_FUSION", 0))
+        int(os.getenv("VLLM_GCU_DEEPSEEK_FUSION", "0"))
+    ),
+    "VLLM_GCU_ENABLE_COMPILE_DUMP": lambda: bool(
+        int(os.getenv("VLLM_GCU_ENABLE_COMPILE_DUMP", "0"))
     ),
 }
 
