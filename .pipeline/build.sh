3be9333e12071a3bf532bfcfe31b6edd3a8d8ba2
鑫 王 <felix.wang@enflame-tech.com>
2025-06-06 18:02:07 +0800

    Revert "[Feature](INF-2947) vllm: donot use last known good in ci"
    This reverts commit b8ff4c5153079661b080986b02fd25973c98b6d7.
    
    Reason for revert: 性能下降
    
    Change-Id: I37477f5b43bbb1f2844e1fe5a5ff299e2ac04719

--- .pipeline/build.sh
+++ .pipeline/build.sh
@@ -16,22 +16,15 @@ function arm_normal_build() {
   epkg get -s buildtree --check-unique --check-missing normal_arm_all_buildtree
 }
 
+function x86_normal_ci_build() {
+  x86_normal_build
+}
+
 function x86_normal_build() {
   echo "Current build job: $FUNCNAME"
   echo `pwd`
   sudo python3.12 -m pip install --index-url http://data-oceanus.enflame.cn/artifactory/api/pypi/pypi_virtual/simple --trusted-host data-oceanus.enflame.cn torch==$TORCH_VERSION+cpu patch pyyaml packaging
   cmake ${project_name} --preset ci_all -B cmake_build
-  cd cmake_build
-  ninja -j${cpu_count} install
-  ninja -j${cpu_count} package_all
-  epkg get -s buildtree --check-unique --check-missing normal_all_buildtree
-}
-
-function x86_normal_ci_build() {
-  echo "Current build job: $FUNCNAME"
-  echo `pwd`
-  sudo python3.12 -m pip install --index-url http://data-oceanus.enflame.cn/artifactory/api/pypi/pypi_virtual/simple --trusted-host data-oceanus.enflame.cn torch==$TORCH_VERSION+cpu patch pyyaml packaging
-  cmake ${project_name} --preset ci_all -B cmake_build -DINCLUDE_TOPS=ON
   cd cmake_build
   ninja -j${cpu_count} install
   ninja -j${cpu_count} package_all
