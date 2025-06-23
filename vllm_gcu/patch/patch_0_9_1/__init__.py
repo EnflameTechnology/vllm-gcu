aa7dc6b915cac07847bb495ea2c611e75d0fc1c7
tianyu.jiang <tianyu.jiang@enflame-tech.com>
2025-06-23 13:31:48 +0800

    [Feature](ARD-3071) vllm: upgrade
    Root Cause:
            patch entrypoints in patch
    Solution:
            to avoid partial import
    Test:
            S60/L600
    Impact area:
            vllm
    Fix status:
            [3/N]
    
    Change-Id: Id9b452b0d9b9125765e34c7cf68bd993d48d7149

--- vllm_gcu/patch/patch_0_9_1/__init__.py
+++ vllm_gcu/patch/patch_0_9_1/__init__.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python
 # coding=utf-8
-import vllm_gcu.patch.patch_0_9_1.compilation_backends
+import vllm_gcu.patch.patch_0_9_1.entrypoints  # noqa
+import vllm_gcu.patch.patch_0_9_1.compilation_backends  # noqa
+import vllm_gcu.patch.patch_0_9_1.utils  # noqa
