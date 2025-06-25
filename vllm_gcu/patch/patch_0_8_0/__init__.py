d9357ba6b9ed0e42d3eb209d04cafd5b7b3d0ef4
xin.guo <xin.guo@enflame-tech.com>
2025-06-25 17:48:14 +0800

    [Workaround](ARD-2916) vllm_gcu: fix inproper noop eliminate
    Root Cause:
            vllm noop pass use torch.SymBool, which may return false positive
    Solution:
            use sympy instead(more strict), may cause some opt break
    Test:
            gcu
    Impact area:
            compile
    Fix status:
            Done
    
    Change-Id: Icdb126f0a3fb53173c7d9a18f4d3f834bb56ae97

--- vllm_gcu/patch/patch_0_8_0/__init__.py
+++ vllm_gcu/patch/patch_0_8_0/__init__.py
@@ -3,3 +3,4 @@ import vllm_gcu.patch.patch_0_8_0.moe_layer  # noqa
 import vllm_gcu.patch.patch_0_8_0.flash_attn  # noqa
 import vllm_gcu.patch.patch_0_8_0.compiler_interface  # noqa
 import vllm_gcu.patch.patch_0_8_0.utils  # noqa
+import vllm_gcu.patch.patch_0_8_0.noop_elimination  # noqa
