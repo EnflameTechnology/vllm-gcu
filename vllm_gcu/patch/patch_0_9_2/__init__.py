1dd95f55e72ee4f155048afaee5b5c3b25f60ea2
tianyu.jiang <tianyu.jiang@enflame-tech.com>
2025-07-09 16:56:50 +0800

    [Feature](ARD-2987) vllm: upgrade patch 0.9.2
    Root Cause:
            0.9.2 has been releases
    Solution:
            patch 0.9.2
    Test:
            S60
    Impact area:
            vLLM
    Fix status:
            Done
    
    Change-Id: I71d1823879f92910d27a370be21c8dc83a7a6d85

---
+++ vllm_gcu/patch/patch_0_9_2/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# coding=utf-8
+import vllm_gcu.patch.patch_0_9_2.entrypoints  # noqa
+import vllm_gcu.patch.patch_0_9_2.compilation_backends  # noqa
+import vllm_gcu.patch.patch_0_9_2.utils  # noqa
