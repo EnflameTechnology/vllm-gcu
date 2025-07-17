23e8c95fd313a87bc0d2bf2d2721c722a47e9f30
陶轩 <sean.tao@enflame-tech.com>
2025-07-17 08:50:20 +0000

    [Feature](ARD-3136) vllm_gcu: add mla attention backend for vllm V1
    Root Cause:
            need mla attention backend for vllm v1
    Solution:
            as title
    Test:
            local tested
    Impact area:
            vllm v1
    Fix status:
            done
    
    Change-Id: I99772a13bb408d06858b86744f46486ea73788e7

---
+++ vllm_gcu/attention/backends/mla_v1.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from dataclasses import dataclass
+from typing import Any, ClassVar, Optional
+
+import torch
+
+from vllm.attention.backends.abstract import (AttentionMetadata, AttentionType,
+                                              is_quantized_kv_cache)
+from vllm.attention.ops.flashmla import (flash_mla_with_kvcache,
+                                         get_mla_metadata,
+                                         is_flashmla_supported)
+from vllm.logger import init_logger
+from vllm.v1.attention.backends.mla.common import (MLACommonBackend,
+                                                   MLACommonDecodeMetadata,
+                                                   MLACommonImpl,
+                                                   MLACommonMetadata,
+                                                   MLACommonMetadataBuilder)
+from vllm.v1.kv_cache_interface import AttentionSpec
+from vllm.v1.worker.block_table import BlockTable
+
+import vllm_gcu.kernels._custom_ops as ops
+
+logger = init_logger(__name__)
+
+
+class GCUMLABackend(MLACommonBackend):
+    @staticmethod
+    def get_name() -> str:
+        return "GCU_MLA_VLLM_V1"
+
+    @staticmethod
+    def get_metadata_cls() -> type["GCUMLAMetadata"]:
+        return GCUMLAMetadata
+
+    @staticmethod
+    def get_builder_cls() -> type["GCUMLAMetadataBuilder"]:
+        return GCUMLAMetadataBuilder
+
+    @staticmethod
+    def get_impl_cls() -> type["GCUMLAImpl"]:
+        return GCUMLAImpl
+
+
+@dataclass
+class GCUMLADecodeMetadata(MLACommonDecodeMetadata):
+    max_query_len: int
+
+
+@dataclass
+class GCUMLAMetadata(MLACommonMetadata[GCUMLADecodeMetadata]):
+    pass
+
+
+class GCUMLAMetadataBuilder(MLACommonMetadataBuilder[GCUMLAMetadata]):
+
+    def _build_decode(self, block_table_tensor: torch.Tensor,
+                      seq_lens: torch.Tensor):
+        return GCUMLADecodeMetadata(
+            block_table=block_table_tensor,
+            seq_lens=seq_lens,
+            max_query_len=seq_lens.max().item()
+        )
+
+
+class GCUMLAImpl(MLACommonImpl[GCUMLAMetadata]):
+    def __init__(
+            self,
+            num_heads: int,
+            head_size: int,
+            scale: float,
+            num_kv_heads: int,
+            alibi_slopes: Optional[list[float]],
+            sliding_window: Optional[int],
+            kv_cache_dtype: str,
+            blocksparse_params: Optional[dict[str, Any]],
+            logits_soft_cap: Optional[float],
+            attn_type: str,
+            kv_sharing_target_layer_name: Optional[str],
+            # MLA Specific Arguments
+            **mla_args) -> None:
+        from flash_attn.vllm_flash_attn import flash_attn_varlen_func
+
+        super().__init__(num_heads, head_size, scale, num_kv_heads,
+                         alibi_slopes, sliding_window, kv_cache_dtype,
+                         blocksparse_params, logits_soft_cap, attn_type,
+                         kv_sharing_target_layer_name, **mla_args)
+
+        self.flash_attn_varlen_func = flash_attn_varlen_func
+
+    def _forward_decode(
+        self,
+        ql_nope: torch.Tensor,
+        q_pe: torch.Tensor,
+        kv_c_and_k_pe_cache: torch.Tensor,
+        attn_metadata: GCUMLAMetadata,
+    ) -> torch.Tensor:
+        assert kv_c_and_k_pe_cache.numel() > 0
+        if self.kv_cache_dtype.startswith("fp8"):
+            raise NotImplementedError("FP8 MLA not yet supported")
+
+        decode_meta = attn_metadata.decode
+        assert decode_meta is not None
+
+        B = ql_nope.shape[0]
+
+        q = torch.cat([ql_nope, q_pe], dim=-1)
+        o = torch.zeros(
+            B, self.num_heads, self.kv_lora_rank, dtype=q.dtype, device=q.device
+        )
+
+        ops.paged_attention_v1(
+            out=o,
+            query=q,
+            key_cache=kv_c_and_k_pe_cache,
+            value_cache=None,
+            num_kv_heads=1,
+            scale=self.scale,
+            block_tables=decode_meta.block_table,
+            seq_lens=decode_meta.seq_lens,
+            block_size=kv_c_and_k_pe_cache.size(1),
+            max_seq_len=decode_meta.max_query_len,
+            alibi_slopes=None,
+            kv_cache_dtype=self.kv_cache_dtype,
+            k_scale_float=1.0,
+            v_scale_float=1.0,
+            out_scales=None,
+        )
+
+        return self._v_up_proj(o)
