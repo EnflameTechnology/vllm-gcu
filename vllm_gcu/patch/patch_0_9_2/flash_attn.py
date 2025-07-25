c32ad18ad154f4dcbc3b32bd5ab86e6abfa2bc55
tianyu.jiang <tianyu.jiang@enflame-tech.com>
2025-07-25 17:20:17 +0800

    [Fix](ARD-2987) vllm: fix v1 mla merge_attn_states & support full graph
    Root Cause:
            as title
    Solution:
            as title
    Test:
            Libra
    Impact area:
            vllm v1
    Fix status:
            Done
    
    Change-Id: I319bfac3fd4912f1dcdfb483671b032d6f502231

--- vllm_gcu/patch/patch_0_9_2/flash_attn.py
+++ vllm_gcu/patch/patch_0_9_2/flash_attn.py
@@ -1,5 +1,4 @@
 from unittest.mock import patch
-import torch
 from vllm_gcu.kernels._custom_ops import merge_attn_states
 from vllm._custom_ops import reshape_and_cache_flash
 try:
@@ -17,26 +16,7 @@ def flash_attn_supports_fp8() -> bool:
     return True
 
 
-# WA for https://github.com/vllm-project/vllm/pull/10989
-def reshape_and_cache_flash(
-    key,
-    value,
-    key_cache,
-    value_cache,
-    slot_mapping,
-    kv_cache_dtype,
-    k_scale,
-    v_scale,
-) -> None:
-
-    key = key[:slot_mapping.shape[0]]
-    value = value[:slot_mapping.shape[0]]
-    torch.ops._C_cache_ops.reshape_and_cache_flash(key, value, key_cache,
-                                                   value_cache, slot_mapping,
-                                                   kv_cache_dtype, k_scale,
-                                                   v_scale)
-
-
+patch("vllm.attention.ops.merge_attn_states.merge_attn_states", merge_attn_states).start()
 patch("vllm.attention.utils.fa_utils.get_flash_attn_version", get_flash_attn_version).start()
 patch("vllm.attention.backends.flash_attn.get_flash_attn_version", get_flash_attn_version).start()
 patch("vllm.attention.backends.flash_attn.flash_attn_supports_fp8", flash_attn_supports_fp8).start()
@@ -46,6 +26,7 @@ patch("vllm.attention.backends.flash_attn.flash_attn_with_kvcache", flash_attn_w
 patch("vllm.v1.attention.backends.flash_attn.get_flash_attn_version", get_flash_attn_version).start()
 patch("vllm.v1.attention.backends.flash_attn.flash_attn_supports_fp8", flash_attn_supports_fp8).start()
 patch("vllm.v1.attention.backends.flash_attn.merge_attn_states", merge_attn_states).start()
+patch("vllm.v1.attention.backends.mla.common.merge_attn_states", merge_attn_states).start()
 
 import vllm.v1.attention.backends.flash_attn  # noqa
 setattr(vllm.v1.attention.backends.flash_attn, "flash_attn_varlen_func", flash_attn_varlen_func)
