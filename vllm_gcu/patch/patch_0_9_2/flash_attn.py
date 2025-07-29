0ea161f23e0876c4e096fab5d8086e88cd4d1830
tianyu.jiang <tianyu.jiang@enflame-tech.com>
2025-07-29 16:10:50 +0800

    [Feature](ARD-3071) vllm: support FA3 fullgraph
    Root Cause:
            use fa3 as default
    Solution:
            fullgraph needs fa3
    Test:
            libra
    Impact area:
            vllm
    Fix status:
            Done
    
    Change-Id: I606318f27c56bce5ab320648525031932985cf49

--- vllm_gcu/patch/patch_0_9_2/flash_attn.py
+++ vllm_gcu/patch/patch_0_9_2/flash_attn.py
@@ -1,33 +1,75 @@
 from unittest.mock import patch
 from vllm_gcu.kernels._custom_ops import merge_attn_states
 from vllm._custom_ops import reshape_and_cache_flash
+
 try:
-    from flash_attn.vllm_flash_attn import flash_attn_varlen_func, flash_attn_with_kvcache
+    from flash_attn.vllm_flash_attn import (
+        flash_attn_varlen_func,
+        flash_attn_with_kvcache,
+        get_scheduler_metadata,
+    )
 except ImportError:
     flash_attn_varlen_func = None
     flash_attn_with_kvcache = None
 
 
 def get_flash_attn_version(requires_alibi: bool = False):
-    return 2
+    return 3
 
 
 def flash_attn_supports_fp8() -> bool:
     return True
 
 
-patch("vllm.attention.ops.merge_attn_states.merge_attn_states", merge_attn_states).start()
-patch("vllm.attention.utils.fa_utils.get_flash_attn_version", get_flash_attn_version).start()
-patch("vllm.attention.backends.flash_attn.get_flash_attn_version", get_flash_attn_version).start()
-patch("vllm.attention.backends.flash_attn.flash_attn_supports_fp8", flash_attn_supports_fp8).start()
-patch("vllm.attention.backends.flash_attn.flash_attn_varlen_func", flash_attn_varlen_func).start()
-patch("vllm.attention.backends.flash_attn.flash_attn_with_kvcache", flash_attn_with_kvcache).start()
+patch(
+    "vllm.attention.utils.fa_utils.get_flash_attn_version", get_flash_attn_version
+).start()
+patch(
+    "vllm.attention.backends.flash_attn.get_flash_attn_version", get_flash_attn_version
+).start()
+patch(
+    "vllm.attention.backends.flash_attn.flash_attn_supports_fp8",
+    flash_attn_supports_fp8,
+).start()
+patch(
+    "vllm.attention.backends.flash_attn.flash_attn_varlen_func", flash_attn_varlen_func
+).start()
+patch(
+    "vllm.attention.backends.flash_attn.flash_attn_with_kvcache",
+    flash_attn_with_kvcache,
+).start()
 
-patch("vllm.v1.attention.backends.flash_attn.get_flash_attn_version", get_flash_attn_version).start()
-patch("vllm.v1.attention.backends.flash_attn.flash_attn_supports_fp8", flash_attn_supports_fp8).start()
-patch("vllm.v1.attention.backends.flash_attn.merge_attn_states", merge_attn_states).start()
-patch("vllm.v1.attention.backends.mla.common.merge_attn_states", merge_attn_states).start()
+patch(
+    "vllm.v1.attention.backends.flash_attn.get_flash_attn_version",
+    get_flash_attn_version,
+).start()
+patch(
+    "vllm.v1.attention.backends.flash_attn.flash_attn_supports_fp8",
+    flash_attn_supports_fp8,
+).start()
+patch(
+    "vllm.v1.attention.backends.flash_attn.merge_attn_states", merge_attn_states
+).start()
+patch(
+    "vllm.v1.attention.backends.mla.common.merge_attn_states", merge_attn_states
+).start()
 
 import vllm.v1.attention.backends.flash_attn  # noqa
-setattr(vllm.v1.attention.backends.flash_attn, "flash_attn_varlen_func", flash_attn_varlen_func)
-setattr(vllm.v1.attention.backends.flash_attn, "reshape_and_cache_flash", reshape_and_cache_flash)
+from vllm.v1.attention.backends.flash_attn import FlashAttentionMetadataBuilder
+
+setattr(
+    vllm.v1.attention.backends.flash_attn,
+    "flash_attn_varlen_func",
+    flash_attn_varlen_func,
+)
+setattr(
+    vllm.v1.attention.backends.flash_attn,
+    "reshape_and_cache_flash",
+    reshape_and_cache_flash,
+)
+setattr(
+    vllm.v1.attention.backends.flash_attn,
+    "get_scheduler_metadata",
+    get_scheduler_metadata,
+)
+FlashAttentionMetadataBuilder.full_cudagraph_supported = True
