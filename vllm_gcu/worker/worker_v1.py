529eb45776ccad97bf4224fef7f885a8c0c602f5
xin.guo <xin.guo@enflame-tech.com>
2025-09-01 11:56:36 +0800

    [Fix](ARD-3071) vllm_gcu: worker import distributed
    Root Cause:
            some worker process not import vllm_gcu.distributed when pd disaggregate
    Solution:
            add import
    Test:
            gcu
    Impact area:
            pd disaggregate
    Fix status:
            Done
    
    Change-Id: Ibcce0860be2110fed5b3b4df160ecd382fbf5d90

--- vllm_gcu/worker/worker_v1.py
+++ vllm_gcu/worker/worker_v1.py
@@ -219,6 +219,7 @@ class GCUWorker(Worker):
         import vllm_gcu.kernels  # noqa: F401
         import vllm_gcu.compilation  # noqa: F401
         import vllm_gcu.patch  # noqa: F401
+        import vllm_gcu.distributed  # noqa
         if gcu_envs.VLLM_GCU_RANK_LOG_PATH:
             # before init dist, since we want to split eccl init logs
             dp_rank = vllm_config.parallel_config.data_parallel_rank
