f9753b07ce41149dea3797cb2de3446bbff7c7bc
hongze <hongze.tian@enflame-tech.com>
2025-09-30 11:26:43 +0800

    [Feature](TR-61496) ard: support ds mtp w4a8
    Root Cause:
            as title
    Solution:
            as title
    Test:
            gcu
    Impact area:
            mtp
    Fix status:
            N/A
    
    Change-Id: I09b887eda72cae784aeeaba6a99126ad62571d69

--- vllm_gcu/models/deepseek_mtp/deepseek_mtp.py
+++ vllm_gcu/models/deepseek_mtp/deepseek_mtp.py
@@ -249,7 +249,7 @@ class DeepSeekMTP(nn.Module):
                 # Skip loading extra bias for GPTQ models.
                 if name.endswith(".bias") and name not in params_dict:
                     continue
-
+                if name not in params_dict: continue
                 param = params_dict[name]
                 weight_loader = param.weight_loader
                 weight_loader(param, loaded_weight, shard_id)
@@ -260,7 +260,7 @@ class DeepSeekMTP(nn.Module):
                     if weight_name not in name:
                         continue
                     name = name.replace(weight_name, param_name)
-
+                    if name not in params_dict: continue
                     param = params_dict[name]
                     weight_loader = param.weight_loader
                     weight_loader(param,
@@ -279,7 +279,7 @@ class DeepSeekMTP(nn.Module):
                     if (spec_layer != self.model.mtp_start_layer_idx
                             and ".layers" not in name):
                         continue
-
+                    if name not in params_dict: continue
                     param = params_dict[name]
                     weight_loader = getattr(param, "weight_loader",
                                             default_weight_loader)
