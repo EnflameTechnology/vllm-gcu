82d66459a4f07d8522cce4e207a13cb88d8891aa
hongze <hongze.tian@enflame-tech.com>
2025-10-02 17:27:58 +0800

    [Feature](TR-61496) ard: support prepare input triton
    Root Cause:
            1
    Solution:
            1
    Test:
            1
    Impact area:
            1
    Fix status:
            N/A
    
    Change-Id: If88454142543287ff90076870021a56791929217

--- vllm_gcu/patch/patch_0_9_2/__init__.py
+++ vllm_gcu/patch/patch_0_9_2/__init__.py
@@ -9,7 +9,6 @@ import vllm_gcu.patch.patch_0_9_2.forward_context  # noqa
 import vllm_gcu.patch.patch_0_9_2.noop_elimination  # noqa
 import vllm_gcu.patch.patch_0_9_2.rejection_sampler  # noqa
 import vllm_gcu.patch.patch_0_9_2.flashmla  # noqa
-import vllm_gcu.patch.patch_0_9_2.eagle  # noqa
 import vllm_gcu.patch.patch_0_9_2.nixl_connector  # noqa
 import vllm_gcu.patch.patch_0_9_2.compressed_tensor  # noqa
 import vllm_gcu.patch.patch_0_9_2.engine  # noqa
