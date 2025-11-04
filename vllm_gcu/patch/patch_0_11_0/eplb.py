b72d6dd1427659afcf15455b3ec220e9bb0b2da4
陶轩 <sean.tao@enflame-tech.com>
2025-11-04 09:16:49 +0000

    [Refactor](TR-62069) eplb: merge 4 tensor of each expert into 1 to                            improve xfer performance
    Root Cause:
            each expert xfer 4 tensors, batch_isend_irecv very slow
    Solution:
            merge 4 tensors into 1 to reduce xfer amount
    Test:
            local tested
    Impact area:
            eplb
    Fix status:
            done
    
    Change-Id: I766733c6749b90e66920c6f04a713f2b00432b91

--- vllm_gcu/patch/patch_0_11_0/eplb.py
+++ vllm_gcu/patch/patch_0_11_0/eplb.py
@@ -1,4 +1,8 @@
 from unittest.mock import patch
 from vllm_gcu.distributed.eplb.eplb_state import step
+from vllm_gcu.distributed.eplb.rebalance_execute import rearrange_expert_weights_inplace, shuffle_layer
 
 patch("vllm.distributed.eplb.eplb_state.EplbState.step", step).start()
+patch("vllm.distributed.eplb.rebalance_execute.rearrange_expert_weights_inplace", rearrange_expert_weights_inplace).start()
+patch("vllm.distributed.eplb.eplb_state.rearrange_expert_weights_inplace", rearrange_expert_weights_inplace).start()
+patch("vllm.distributed.eplb.rebalance_execute.shuffle_layer", shuffle_layer).start()
