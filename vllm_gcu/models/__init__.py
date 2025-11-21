b6f58c5ccd436c7dafc4f08524a77721999ce474
sp.yancong.liu <sp.yancong.liu@enflame-tech.com>
2025-11-21 07:17:38 +0000

    [Fix](TR-62211) vllm: fix bug for qwen2-vl
    Root Cause:
            fix bug for qwen2-vl
    Solution:
            add to device
    Test:
            done
    Impact area:
            vllm
    Fix status:
            N/A
    
    Signed-off-by: sp.yancong.liu <sp.yancong.liu@enflame-tech.com>
    Change-Id: I5e3d1451d5d0daed9140b9f5105e00cfb540dd42

--- vllm_gcu/models/__init__.py
+++ vllm_gcu/models/__init__.py
@@ -36,3 +36,4 @@ def register_custom_models():
     ModelRegistry.register_model("HunYuanDenseV1ForCausalLM", "vllm_gcu.models.hunyuan_v1:HunYuanDenseV1ForCausalLM")
     ModelRegistry.register_model("HunYuanMoEV1ForCausalLM", "vllm_gcu.models.hunyuan_v1:HunYuanMoEV1ForCausalLM")
     ModelRegistry.register_model("KeyeVL1_5ForConditionalGeneration", "vllm_gcu.models.keye_vl:KeyeVL1_5ForConditionalGeneration")#gitleaks:allow
+    ModelRegistry.register_model("Qwen2VLForConditionalGeneration", "vllm_gcu.models.qwen2_vl:Qwen2VLForConditionalGeneration")
