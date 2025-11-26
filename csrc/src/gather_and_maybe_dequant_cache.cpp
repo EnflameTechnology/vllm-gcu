137b25c22e32f9ec615007d4afb03fc3de825d55
sp.yancong.liu <sp.yancong.liu@enflame-tech.com>
2025-11-26 09:12:48 +0000

    [Feature](TR-62639) vllm: use gather_cache on S60 replace gather_and_maybe_dequant_cache
    Root Cause:
            use gather_cache on S60 replace gather_and_maybe_dequant_cache
    Solution:
            use gather_cache
    Test:
            done
    Impact area:
            vllm
    Fix status:
            N/A
    
    Signed-off-by: sp.yancong.liu <sp.yancong.liu@enflame-tech.com>
    Change-Id: I758a4c00dd5eff63a2a89cc03685bd4ca958b986

--- csrc/src/gather_and_maybe_dequant_cache.cpp
+++ csrc/src/gather_and_maybe_dequant_cache.cpp
@@ -5,18 +5,19 @@
 
 #include <topsaten/topsaten_vllm.h>
 
+#include "tops_extension/tops/Context.h"
 #include "tops_extension/torch/GCUAten.h"
 #include "torch_gcu.h"
 
 namespace vllm_gcu::llm_ops {
 void gather_and_maybe_dequant_cache(
-    const at::Tensor& src_cache, const at::Tensor& dst,
-    const at::Tensor& block_table, const at::Tensor& cu_seq_lens,
+    const at::Tensor &src_cache, const at::Tensor &dst,
+    const at::Tensor &block_table, const at::Tensor &cu_seq_lens,
     int64_t batch_size, c10::string_view kv_cache_dtype,
-    const at::Tensor& scale, const c10::optional<at::Tensor> &seq_starts) {
+    const at::Tensor &scale, const c10::optional<at::Tensor> &seq_starts) {
   const torch_gcu::OptionalGCUGuard device_guard(device_of(dst));
   const topsStream_t stream = torch_gcu::getCurrentGCUStream();
-  const char* kv_dtype = kv_cache_dtype.data();
+  const char *kv_dtype = kv_cache_dtype.data();
 
   at::Tensor seq_starts_tensor;
   if (seq_starts.has_value()) {
@@ -30,10 +31,15 @@ void gather_and_maybe_dequant_cache(
     scale_tensor = scale;
   }
 
-  ATEN_ATENOP_CHECK(
-  ATEN_ATENOP_CALL(
-      topsvllm::topsvllmGatherAndMaybeDequantCache)(
-    dst, src_cache, block_table, cu_seq_lens, batch_size, kv_dtype,
-    scale_tensor, seq_starts_tensor, stream));
+  if (get_device_capability() == 3) {
+    ATEN_ATENOP_CHECK(ATEN_ATENOP_CALL(topsvllm::topsvllmGatherCache)(
+        src_cache, dst, block_table, cu_seq_lens, batch_size, seq_starts_tensor,
+        stream));
+  } else {
+    ATEN_ATENOP_CHECK(
+        ATEN_ATENOP_CALL(topsvllm::topsvllmGatherAndMaybeDequantCache)(
+            dst, src_cache, block_table, cu_seq_lens, batch_size, kv_dtype,
+            scale_tensor, seq_starts_tensor, stream));
   }
-}  // namespace vllm_gcu::llm_ops
+}
+} // namespace vllm_gcu::llm_ops
