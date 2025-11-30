36bf510004bcc57c2729b3e45747a7f468406122
knight.zhang <knight.zhang@enflame-tech.com>
2025-11-30 07:36:13 +0000

    [Feature](ARD-3007) vllm_gcu: remove slice/slice_scatter op when shape all equal
    Root Cause:
            torch.Size([-4*((s72//(-4))),4096])==torch.Size([s72, 4096]) in
    torch.compile
    Solution:
            follow https://github.com/vllm-project/vllm/pull/26394
    	use (dim == i_dim)._sympy_() == sympy.true
    Test:
            qwen3,dp2tp4sp4ep8,cudagraph_size=7
    Impact area:
            vllm gcu
    Fix status:
            done
    
    Change-Id: Ibf9477581e2aa64d62a4048debb455961ccbd554

--- vllm_gcu/compilation/noop_elimination.py
+++ vllm_gcu/compilation/noop_elimination.py
@@ -1,14 +1,82 @@
+from collections.abc import Iterable
+import sympy
 import torch
+from torch import SymInt
 from typing import Union
 from vllm.compilation.noop_elimination import NoOpEliminationPass
-
+from vllm.compilation.fx_utils import is_func
+from vllm.compilation.vllm_inductor_pass import VllmInductorPass
+from vllm.logger import init_logger
+logger = init_logger(__name__)
 
 class CustomNoOpEliminationPass(NoOpEliminationPass):
-    def reshape_dims_equivalent(self, dim: Union[int, torch.fx.Node],
-                                i_dim: Union[int, torch.SymInt]) -> bool:
-        # Case 1 and 2
-        if dim == i_dim or dim == -1:
-            return True
-        # Case 3
-        return isinstance(dim, torch.fx.Node) and (dim.meta["val"]
-                                                   == i_dim)._sympy_()
+
+    @VllmInductorPass.time_and_log
+    def __call__(self, graph: torch.fx.Graph):
+        count = 0
+        # Remove no-op reshapes/views:
+        for node in graph.nodes:
+            if is_func(node, torch.ops.aten.reshape.default):
+                # Case 1: rewrite reshape chains to reshapes on the base tensor
+                input = node.args[0]
+                # If the input is a reshape, rebind to that node
+                if is_func(input, torch.ops.aten.reshape.default):
+                    # The new input is guaranteed not to be a reshape,
+                    # because we process nodes in order
+                    node.update_arg(0, input.args[0])
+                    if len(input.users) == 0:
+                        graph.erase_node(input)
+                        count += 1
+
+            # remove reshape/slice if it produces the original shape
+            if is_func(node, torch.ops.aten.reshape.default) or is_func(
+                node, torch.ops.aten.slice.Tensor
+            ):
+                input = node.args[0]
+                input_shape = input.meta["val"].shape
+                output_shape = node.meta["val"].shape
+                if self.all_dims_equivalent(input_shape, output_shape):
+                    node.replace_all_uses_with(input)
+                    graph.erase_node(node)
+                    count += 1
+            elif is_func(node, torch.ops.aten.slice_scatter.default):
+                base, view, dim_index, start, end = node.args[:5]
+                base_shape = base.meta["val"].shape
+                view_shape = view.meta["val"].shape
+
+                if self.all_dims_equivalent(base_shape, view_shape):
+                    node.replace_all_uses_with(view)
+                    graph.erase_node(node)
+                    count += 1
+
+        logger.debug("Removed %s no-op reshapes and slices", count)
+
+    # ---------------------- Shape comparison helpers ----------------------
+    def dims_equivalent(self, dim: int | SymInt, i_dim: int | SymInt) -> bool:
+        """
+        This function checks if two dimensions are equivalent.
+        :param dim: The dimension arg to reshape/slice
+        :param i_dim: The corresponding dimension in the input tensor
+        :return: Are the dimensions equivalent?
+
+        There are two cases in which the dimensions are equivalent:
+        1. The dimensions are equal (both integers)
+        2. The dimensions both correspond to the same SymInt
+        """
+        # Case 1
+        if isinstance(i_dim, int) and isinstance(dim, int):
+            return dim == i_dim
+        # Case 2
+        if isinstance(i_dim, SymInt) and isinstance(dim, SymInt):
+            return (dim == i_dim)._sympy_() == sympy.true
+        return False
+
+    def all_dims_equivalent(
+        self, dims: Iterable[int | SymInt], i_dims: Iterable[int | SymInt]
+    ) -> bool:
+        dims_ = list(dims)
+        i_dims_ = list(i_dims)
+        if len(dims_) != len(i_dims_):
+            # Different ranks can't be equivalent
+            return False
+        return all(self.dims_equivalent(s, i_s) for s, i_s in zip(dims, i_dims))
