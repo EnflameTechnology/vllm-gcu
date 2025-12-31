b79d668a9d9623de5e9316dda4f719ee2153c67d
shaoping.yang <shaoping.yang@enflame-tech.com>
2025-12-31 09:16:57 +0800

    [Test](SQ-11193) vllm: add models unit test and dev test
    Root Cause:
            1. add step3v unit test
            2. add deepseek_v3 unit test
            3. add tokenizers unit test
            4. add dev test
    Solution:
            as title
    Test:
            local test
    Impact area:
            vllm
    Fix status:
            done
    
    Change-Id: I604bbbddaa564a84dd324315f648f4684b4b8b9a

--- tests/ctest_libra.cmake
+++ tests/ctest_libra.cmake
@@ -11,3 +11,25 @@
 #              export TORCHGCU_INDUCTOR_ENABLE=0 && \
 #              export TORCH_ECCL_AVOID_RECORD_STREAMS=1 && \
 #              export TORCH_DEVICE_BACKEND_AUTOLOAD=0 && python -m pytest test_offline_inference.py")
+
+add_py_test (PROJECT libra
+             PLATFORM silicon
+             REGRESSION daily
+             CATEGORY func
+             OS ubuntu
+             MODULE vllm_dev
+             ID 1
+             NAME test_gcu_models
+             COMMAND "cd tests && python -m pytest -sv kernels/test_routing_strategy.py"
+            )
+add_py_test (PROJECT libra
+             PLATFORM silicon
+             REGRESSION daily
+             CATEGORY func
+             OS ubuntu
+             MODULE vllm_dev
+             ID 2
+             NAME test_apply_repetition_penalties
+             COMMAND "cd tests && python -m pytest -sv kernels/test_apply_repetition_penalties.py"
+            )
+ 
