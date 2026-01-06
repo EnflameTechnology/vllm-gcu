8c84fe2460389b9612af2f0ea1b80f22126bb4ca
harry.zhao <harry.zhao@enflame-tech.com>
2026-01-06 18:53:23 +0800

    [Feature](ARD-3282) Application: add repetition_penalties op for s60
    Root Cause:
            for op fusion in 3.0
    Solution:
            add fusion op
    Test:
            test on s60
    Impact area:
            repeat penalty
    Fix status:
            N/A
    
    Signed-off-by: harry.zhao <harry.zhao@enflame-tech.com>
    Change-Id: I46ad73bc49df5e8588694389b147ede98382598a

--- csrc/src/apply_repetition_penalties.cpp
+++ csrc/src/apply_repetition_penalties.cpp
@@ -18,20 +18,8 @@ void apply_repetition_penalties(at::Tensor& logits,
                                 const at::Tensor& repetition_penalties) {
   const torch_gcu::OptionalGCUGuard device_guard(device_of(logits));
   const topsStream_t stream = torch_gcu::getCurrentGCUStream();
-  if (get_device_capability() == 3) {
-    at::Tensor repetition_penalties_repeat =
-        repetition_penalties.unsqueeze(1).repeat({1, logits.size(1)});
-
-    auto penalties = torch::where(prompt_mask.logical_or(output_mask),
-                                    repetition_penalties_repeat, 1.0);
-
-    auto scaling = torch::where(logits > 0, 1.0 / penalties, penalties);
-    logits.mul_(scaling);
-  } else {
-    ATEN_ATENOP_CHECK(
-        ATEN_ATENOP_CALL(topsvllm::topsvllmApplyRepetitionPenalties)(
-        logits, prompt_mask, output_mask, repetition_penalties));
-  }
+  ATEN_ATENOP_CHECK(
+      ATEN_ATENOP_CALL(topsvllm::topsvllmApplyRepetitionPenalties)(
+      logits, prompt_mask, output_mask, repetition_penalties, stream));
 }
-
 }  // namespace vllm_gcu::llm_ops
