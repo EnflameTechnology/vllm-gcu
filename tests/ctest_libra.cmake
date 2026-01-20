62671ff1262cb5d85b6a424cf58d3afaddff8ed2
shaoping.yang <shaoping.yang@enflame-tech.com>
2026-01-20 11:38:51 +0800

    [Test](SQ-11193) vllm-gcu: add mrotary_embedding test and online dev kernel test
    Root Cause:
            1. add dev kernel test: test_mrotary_embedding.py、test_topk_softmax_renormalize.py
            2. mrotary_embedding: test_mrotary_embedding.py
    Solution:
            as title
    Test:
            local test
    Impact area:
            vllm-gcu
    Fix status:
            done
    
    Change-Id: Ifae5935ee27c8434d656f4242878c73c3f1d1fa9

--- tests/ctest_libra.cmake
+++ tests/ctest_libra.cmake
@@ -1,17 +1,3 @@
-# add_py_test (PROJECT libra
-#              PLATFORM silicon
-#              REGRESSION ci daily
-#              CATEGORY func
-#              OS ubuntu
-#              MODULE vllm
-#              ID 1
-#              NAME test_gcu_models
-#              COMMAND "cd tests && \
-#              export PYTORCH_EFML_BASED_GCU_CHECK=1 && \
-#              export TORCHGCU_INDUCTOR_ENABLE=0 && \
-#              export TORCH_ECCL_AVOID_RECORD_STREAMS=1 && \
-#              export TORCH_DEVICE_BACKEND_AUTOLOAD=0 && python -m pytest test_offline_inference.py")
-
 add_py_test (PROJECT libra
              PLATFORM silicon
              REGRESSION daily
@@ -31,5 +17,15 @@ add_py_test (PROJECT libra
              ID 2
              NAME test_apply_repetition_penalties
              COMMAND "cd tests && python -m pytest -sv kernels/test_apply_repetition_penalties.py"
+            )
+add_py_test (PROJECT libra
+             PLATFORM silicon
+             REGRESSION daily
+             CATEGORY func
+             OS ubuntu
+             MODULE vllm_dev
+             ID 3
+             NAME test_topk_softmax_renormalize
+             COMMAND "cd tests && python -m pytest -sv kernels/test_topk_softmax_renormalize.py"
             )
  
--- tests/ctest_scorpio.cmake
+++ tests/ctest_scorpio.cmake
@@ -1,13 +1,10 @@
-# add_py_test (PROJECT scorpio
-#              PLATFORM s60
-#              REGRESSION ci daily
-#              CATEGORY func
-#              OS ubuntu
-#              MODULE vllm
-#              ID 1
-#              NAME test_gcu_models
-#              COMMAND "cd tests && \
-#              export PYTORCH_EFML_BASED_GCU_CHECK=1 && \
-#              export TORCHGCU_INDUCTOR_ENABLE=0 && \
-#              export TORCH_ECCL_AVOID_RECORD_STREAMS=1 && \
-#              export TORCH_DEVICE_BACKEND_AUTOLOAD=0 && python -m pytest test_offline_inference.py")
+add_py_test (PROJECT scorpio
+             PLATFORM s60
+             REGRESSION daily
+             CATEGORY func
+             OS ubuntu
+             MODULE vllm_dev_kernels
+             ID 1
+             NAME test_mrotary_embedding
+             COMMAND "cd tests && python -m pytest -sv kernels/test_mrotary_embedding.py"
+            )
