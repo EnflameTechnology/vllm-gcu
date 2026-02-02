b5221a57eb380cd34b9b8b9606b822543ab244eb
tong.zhu <tong.zhu@enflame-tech.com>
2026-02-02 13:56:25 +0800

    [Feature](INF-3081) infra: update fa release rules.
    Root Cause:
            support fa torch version
    Solution:
            support fa torch version
    Test:
            ci
    Impact area:
            release
    Fix status:
            done
    
    Change-Id: Ife5083a8adf9631e917173ef7aa44a511f649f43

--- cmake/2nd/flash_attnention_version.cmake
+++ cmake/2nd/flash_attnention_version.cmake
@@ -1,3 +1,19 @@
+
+function(get_version _version _torch_version)
+    set(_v "${_VERSION}")
+    if(NOT _torch_version IN_LIST fa_torch_version_list)
+        set(${_version} "" PARENT_SCOPE)
+        return()
+    endif()
+    foreach(_tv IN LISTS fa_torch_version_list)
+        string(REPLACE "torch.${_tv}" "torch.${_torch_version}" _v "${_v}")
+    endforeach()
+    set(${_version} ${_v} PARENT_SCOPE)
+endfunction()
+
+set(fa_torch_version_list 2.3.0 2.5.1 2.6.0 2.7.0 2.8.0 2.9.0)
+
+
 if ("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "x86_64")
     set(PACKAGE_CMDS "mkdir -p ${CMAKE_FPKG_PYTHON_PACKAGES}; mv /FILE/ -t ${CMAKE_FPKG_PYTHON_PACKAGES}")
     set(PACKAGE_FILES "${CMAKE_FPKG_PYTHON_PACKAGES}//FILE/")
@@ -7,8 +23,9 @@ if ("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "x86_64")
     set(_SEMI_NAME  "")
     set(_BRANCH master)
     set(_pvs "310" "312")
+    get_version(_TORCH28_VERSION 2.8.0)
     foreach(_pv IN LISTS _pvs)
-        set(_link module_package/flash-attention/${_COMMIT_ID}/${_MNAME}${_SEMI_NAME}-${_VERSION}-cp${_pv}-cp${_pv}-linux_x86_64.whl)
+        set(_link module_package/flash-attention/${_COMMIT_ID}/${_MNAME}${_SEMI_NAME}-${_TORCH28_VERSION}-cp${_pv}-cp${_pv}-linux_x86_64.whl)
         fetchFromArtifactory(fetch_${_MNAME}_${_pv}_28
             FILE ${_link}
             PKG_COMMAND ${PACKAGE_CMDS}
