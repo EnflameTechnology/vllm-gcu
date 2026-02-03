9f18bbbb58e670a890e597a59a016d8fde9e08cd
zhengjun.wang <zhengjun.wang@enflame-tech.com>
2026-02-03 10:32:15 +0800

    [Feature](TR-62867) ai-infra: Add DecodeBenchConnector
    Root Cause:
            Enable measurement of decoder performance without prefill
    Solution:
            Add A KV Connector for decode instance performance testing
    Test:
            Done
    Impact area:
            vllm_gcu PD disaggregated
    Fix status:
            Done
    
    Change-Id: I02b9d49f571d32231222cbf82d6aaa72e8e03972

--- vllm_gcu/distributed/kv_transfer/__init__.py
+++ vllm_gcu/distributed/kv_transfer/__init__.py
@@ -10,3 +10,9 @@ KVConnectorFactory.register_connector(
     "vllm_gcu.distributed.kv_transfer.v1.nixl_layerwise_connector",
     "NixlLayerwiseConnector",
 )
+
+KVConnectorFactory.register_connector(
+    "DecodeBenchConnector",
+    "vllm_gcu.distributed.kv_transfer.v1.decode_bench_connector",
+    "DecodeBenchConnector",
+)
